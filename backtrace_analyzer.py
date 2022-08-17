ba9b43e0a056b9748bdb862cd00d53459d83f0d5
dedong-eco-21 <xiededong001211@gmail.com>
2022-08-16 23:00:10 -0400

    Add callsite finding component

--- backtrace_analyzer.py
+++ backtrace_analyzer.py
@@ -7,15 +7,47 @@
 # Input form: 
 # python3 backtrace_analyzer.py <input file> <path to libj9jit29.so> <output file>
 import os
+import subprocess
 import io
 import sys
 
 prefixLength = len("/mnt/nvme/dedong2022/jdk/openj9-openjdk-jdk17")
 
+skip_list = ["allocate", "TypedAllocator", "heap_allocator"]
+catch_list = ["TR_LoopVersioner::emitExpr", 
+"RematTools::walkTreesCalculatingRematSafety", 
+"J9::Compilation::verifyCompressedRefsAnchors", 
+"TR_UseDefInfo::dereferenceDef", 
+"TR_GlobalRegisterAllocator::findLoopsAndCorrespondingAutos",
+"TR_GlobalRegisterAllocator::markAutosUsedIn"]
+def skip_line(line):
+    for seg in skip_list:
+        if line.find(seg) != -1:
+            return True
+    return False
+
+def catch_line(line):
+    for seg in catch_list:
+        if line.find(seg) != -1:
+            return True
+    return False
+
+def get_callsite(backTraceList):
+    trimmed = backTraceList
+    for i in range(1, len(backTraceList)):
+        if (backTraceList[i][1].find("/omr/") != -1 or backTraceList[i][1].find("/openj9/") != -1) and (catch_line(backTraceList[i][1]) or not skip_line(backTraceList[i][1])):
+            trimmed = backTraceList[:i+1]
+            if i < len(backTraceList) - 2:
+                trimmed.append(backTraceList[i+1][1]+'\n')
+            return trimmed
+    return trimmed
+
 # From list of offsets, build dict to translate from offset to lines
 def build_translation_table(executableFilePath, offsetList, funcDict):
-    stream = os.popen("addr2line -e " + executableFilePath + " -f -C " + " ".join(offsetList))
-    output = stream.readlines()
+    args = ["addr2line", "-e", executableFilePath, "-f", "-C"]
+    args += offsetList
+    output = subprocess.run(args, capture_output=True, text=True).stdout
+    output = output.split('\n')[:-1]
     if len(output) != 2 * len(offsetList):
         print("Translation error")
         return {}
@@ -24,14 +56,14 @@ def build_translation_table(executableFilePath, offsetList, funcDict):
     return funcDict
 
 # Read from formatted lines produce list of all regions and all offsets of interest
-def read_regions(lines):
+def read_regions(file):
     regionList = []
     offsetSet = set()
     region = []
     allocationList = []
-    index = 0
-    while index < len(lines):
-        if lines[index][0].isalpha():
+    line = file.readline()
+    while line:
+        if line[0].isalpha():
             if allocationList != []:
                 allocationList.sort(reverse=True, key=lambda x: x[0])
                 region.append(allocationList)
@@ -39,60 +71,74 @@ def read_regions(lines):
                 allocationList = []
             region = []
             # Start of header for a region
-            region.append(lines[index])
-            index += 1
-            if lines[index][0] == '0':
+            region.append(line)
+            line = file.readline()
+            if line[0] == '0':
                 region.append("S")
             else:
                 region.append("H")
-            constructorOffsets = (lines[index].split())[1:-1]
+            constructorOffsets = (line.split())[1:-1]
             region.append(constructorOffsets)
             offsetSet |= set(constructorOffsets)
         else:
             # Allocations in a region
-            allocation = (lines[index].split())[:-1]
+            allocation = (line.split())[:-1]
             allocationSize = int(allocation[0])
             allocationBackTraces = allocation[1:]
             allocationList.append([allocationSize, allocationBackTraces])
             offsetSet |= set(allocationBackTraces)
-        index += 1
+        line = file.readline()
     if allocationList != []:
         allocationList.sort(key=lambda x: x[0], reverse=True, )
         region.append(allocationList)
         regionList.append(region)
     return regionList, list(offsetSet)
 
+# Translate and get callsites
+def get_translated_callsites(regionList, funcDict):
+    translatedRegionList = []
+    for methodCompiled, regionType, constructorBackTrace, allocations in regionList:
+        regionBackTraceList = []
+        for offset in constructorBackTrace:
+            regionBackTraceList.append([funcDict[offset]])
+        allocationList = []
+        for allocationSize, allocationBackTraceOffsets in allocations:
+            allocationBackTraceList = []
+            for offset in allocationBackTraceOffsets:
+                allocationBackTraceList.append([funcDict[offset]])
+                # Here, funcDict's value is a pair of function name and line
+                # We care about the line.
+                allocationBackTraceList = get_callsite(allocationBackTraceList)
+            allocationList.append(allocationSize, allocationBackTraceList)
+        translatedRegionList.append([methodCompiled, regionType, regionBackTraceList, allocationList])
+    return translatedRegionList
+
 # Write to output file of translated regions
-def write_results(regionList, funcDict, file):
+def write_results(regionList, file):
     for methodCompiled, regionType, constructorBackTrace, allocations in regionList:
         file.write(regionType + ": " + methodCompiled)
-        for offset in constructorBackTrace:
-            try:
-                function, line = funcDict[offset]
-                file.write(line[:-1] + "; " + function)
-            except:
-                file.write(offset + " no line found")
+        for function, line in constructorBackTrace:
+                file.write(line[:-1] + "; " + function + '\n')
         for allocationSize, allocationBackTraceOffsets in allocations:
             file.write("Allocated " + str(allocationSize) + " bytes\n")
-            for offset in allocationBackTraceOffsets:
-                try:
-                    function, line = funcDict[offset]
-                    file.write(line[:-1] + "; " + function)
-                except:
-                    file.write(offset + " no line found\n")
+            for function, line in allocationBackTraceOffsets:
+                file.write(line[:-1] + "; " + function + '\n')
         file.write("=== End of a region ===\n")
 
 if __name__ == "__main__":
     fileName = sys.argv[1]
     sys.stderr.write("reading from " + fileName + "\n")
-    input = open(fileName, "r")
-    regionList, offsetList = read_regions(input.readlines())
+    with open(fileName, "r") as input:
+        regionList, offsetList = read_regions(input)
     translationTable = {}
+    sys.stderr.write("Total regions: " + str(len(regionList)) + "\n")
+    sys.stderr.write("Total offsets: " + str(len(offsetList)) + "\n")
     i = 0
-    step = 10
+    step = 10000
     for i in range(0, len(offsetList), step):
         translationTable = build_translation_table(sys.argv[2]+"libj9jit29.so", offsetList[i:i+step], translationTable)
+    regionList = get_translated_callsites(regionList, translationTable)
     fileOut = sys.argv[3]
-    output = open(fileOut, "w")
-    write_results(regionList, translationTable, output)
+    with open(fileOut, "w") as output:
+        write_results(regionList, output)
     
